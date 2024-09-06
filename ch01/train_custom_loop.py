f103d28262bca4ae1560539d0593f225e9baaf9b
keonmo <rjsah80471@gmail.com>
2024-09-06 21:06:44 +0900

    Chapter 1 practice

--- ch01/train_custom_loop.py
+++ ch01/train_custom_loop.py
@@ -1,8 +1,48 @@
 import sys
 sys.path.append('..')
 import numpy as np
-from common.optimizer import SDG
+from common.optimizer import SGD
 from dataset import spiral
 import matplotlib.pyplot as plt
 from two_layer_net import TwoLayerNet
 
+# 1. Set hyperparameter
+max_epoch = 100
+hidden_size = 10
+batch_size = 30
+learning_rate = 0.1
+
+# 2. Load data, initialize model(Two Layer Net), Optimizer
+x, t = spiral.load_data()
+model = TwoLayerNet(input_size=2, hidden_size=hidden_size, output_size=3)
+optimizer = SGD(learning_rate)
+
+# 3. set variables for learning
+data_size = len(x)
+max_iter = data_size//batch_size
+total_loss, loss_count = 0, 0
+loss_list = []
+
+for epoch in range(max_epoch):
+    idx = np.random.permutation(data_size)
+    x = x[idx]
+    t = t[idx]
+
+    for iters in range(max_iter):
+        batch_x = x[iters*batch_size:(iters+1)*batch_size]
+        batch_t = t[iters*batch_size:(iters+1)*batch_size]
+
+        # 4. calculate gradient
+        loss = model.forward(batch_x, batch_t)
+        model.backward()
+        optimizer.update(model.params, model.grads)
+
+        total_loss += loss
+        loss_count += 1
+
+        # 5. print learning status
+        if (iters+1) % 10 == 0:
+            avg_loss = round(total_loss/loss_count,3)
+            print(f'| epoch: {epoch+1} | iters: {iters+1} | loss: {avg_loss}')
+            loss_list.append(avg_loss)
+            total_loss, loss_count = 0, 0
--- common/layers.py
+++ common/layers.py
@@ -38,19 +38,19 @@ class Sigmoid:
 class Affine:
     def __init__(self, W, b):
         self.params = [W, b]
-        self.grads = [np.zeros_like(W). np.zeros_like(b)]
+        self.grads = [np.zeros_like(W), np.zeros_like(b)]
         self.x = None
 
     def forward(self, x):
         W, b = self.params
-        out = np.matmul(x, W) + b
+        out = np.dot(x, W) + b
         self.x = x
         return out
 
     def backward(self, dout):
         W, b = self.params
-        dx = np.matmul(dout, W.T)
-        dW = np.matmul(self.x.T, dout)
+        dx = np.dot(dout, W.T)
+        dW = np.dot(self.x.T, dout)
         db = np.sum(dout, axis=0)
 
         self.grads[0][...] = dW
@@ -60,17 +60,27 @@ class Affine:
 class SoftmaxWithLoss:
     def __init__(self):
         self.params, self.grads = [], []
-        self.x = None
+        self.t = None
         self.y = None
 
     def forward(self, x, t):
-        self.x = x
+        self.t = t
         self.y = softmax(x)
 
         if self.t.size == self.y.size:
             self.t = self.t.argmax(axis=1)
 
         loss = cross_entropy_error(self.y, self.t)
-
         return loss
+
+    def backward(self, dout):
+        batch_size = self.t.shape[0]
+
+        dx = self.y.copy()
+        dx[np.arange(batch_size), self.t] -= 1
+        dx *= dout
+        dx = dx / batch_size
+
+        return dx
+
 
